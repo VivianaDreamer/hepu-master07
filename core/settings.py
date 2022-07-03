470e1859b725b63382e99baed966bc66126c197c
VivianaDreamer <viviana.rojas.b@gmail.com>
2022-07-03 19:53:08 -0400

    Cambie hepu a magnolio

--- core/settings.py
+++ core/settings.py
@@ -18,7 +18,7 @@ SECRET_KEY = config('SECRET_KEY', default="S#perS3crEt_1122")
 DEBUG = config('DEBUG', default=True)
 
 # load production server from .env
-ALLOWED_HOSTS        = ['localhost', '127.0.0.1', config('SERVER', default='127.0.0.1'), 'heputest.herokuapp.com']
+ALLOWED_HOSTS        = ['localhost', '127.0.0.1', config('SERVER', default='127.0.0.1'), 'magnolio.herokuapp.com']
 CSRF_TRUSTED_ORIGINS = ['http://localhost:85', 'http://127.0.0.1','https://' + config('SERVER', default='127.0.0.1')]
 
 # Application definition
